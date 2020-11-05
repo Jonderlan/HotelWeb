6c0f5cb8292739ec5ba2a6cbfa5af2975ef0ab56
Jonderlan <jonderlan@gmail.com>
2020-11-04 21:58:51 -0300

    Primeiro programa WEB

---
+++ HotelWeb/Controllers/ReservaController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelWeb.Controllers {
+    public class ReservaController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}

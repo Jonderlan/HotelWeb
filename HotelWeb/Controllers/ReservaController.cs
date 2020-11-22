5c898ff496401ef3e2f6a95ea9dfdf3f4feb3ee3
Jonderlan <jonderlan@gmail.com>
2020-11-21 22:38:12 -0300

    alteração no context

--- HotelWeb/Controllers/ReservaController.cs
+++ HotelWeb/Controllers/ReservaController.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelWeb.DAL;
 using HotelWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelWeb.Controllers {
     public class ReservaController : Controller {
 
-        private readonly Context _context;
+        private readonly ReservaDAO _reservaDAO;
 
-        public ReservaController(Context context) => _context = context;
+        public ReservaController(ReservaDAO reservaDAO) => _reservaDAO = reservaDAO;
 
         public IActionResult Index() {
 
-            List<Reserva> reservas = _context.Reservas.ToList();
+            List<Reserva> reservas = _reservaDAO.Listar();
             ViewBag.Reservas = reservas;
             ViewBag.QuantidadeReservas = reservas.Count();
             ViewBag.DataHora = DateTime.Now;
             return View();
         }
         public IActionResult Reservar() => View();
+
         [HttpPost]
         public IActionResult Reservar(
             string txtNome, 
@@ -38,16 +40,44 @@ namespace HotelWeb.Controllers {
                 QuantosCriancas = txtQtdCriancas
             };
 
-            _context.Reservas.Add(reserva);
-            _context.SaveChanges();
+            _reservaDAO.Cadastrar(reserva);
             return RedirectToAction("Index","Reserva");
         }
 
         public IActionResult Remover(int id) {
 
             //Completar com o código da remoção.
+            _reservaDAO.Remover(id);
+            return RedirectToAction("Index", "Reserva");    
+        }
+        public IActionResult Alterar(int id) {
+
+            ViewBag.Reserva = _reservaDAO.BuscarPorId(id);
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(
+            int txtId,
+            string txtNome,
+            string txtTipoQuarto,
+            DateTime txtDtaEntrada,
+            DateTime txtDtaSaida,
+            int txtQtdAdultos,
+            int txtQtdCriancas) {
+            Reserva reserva = _reservaDAO.BuscarPorId(txtId);
+        
+            reserva.Nome = txtNome;
+            reserva.TipoQuarto = txtTipoQuarto;
+            reserva.DataEntrada = txtDtaEntrada;
+            reserva.DataSaida = txtDtaSaida;
+            reserva.QuantosAdultos = txtQtdAdultos;
+            reserva.QuantosCriancas = txtQtdCriancas;
+
+
+            _reservaDAO.Alterar(reserva);
             return RedirectToAction("Index", "Reserva");
         }
-        
+
     }
 }
---
+++ HotelWeb/DAL/ReservaDAO.cs
@@ -0,0 +1,29 @@
+﻿using HotelWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelWeb.DAL {
+    public class ReservaDAO {
+
+        private readonly Context _context;
+        public ReservaDAO(Context context) => _context = context;
+
+        public List<Reserva> Listar() => _context.Reservas.ToList();
+
+        public Reserva BuscarPorId(int id) => _context.Reservas.Find(id);
+        public void Cadastrar(Reserva reserva) {
+            _context.Reservas.Add(reserva);
+            _context.SaveChanges();
+        }
+        public void Remover(int id) {
+            _context.Reservas.Remove(BuscarPorId(id));
+            _context.SaveChanges();
+        }
+        public void Alterar(Reserva reserva) {
+            _context.Reservas.Update(reserva);
+            _context.SaveChanges();
+        }
+    }
+}
